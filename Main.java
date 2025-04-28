19fdcd42eb89d661650a2f72dcc40e3176244a16
Ilse <iruedah1205@gmail.com>
2025-04-27 20:34:40 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        // Crear un nuevo objeto de tipo Estudiante
+        Estudiante estudiante1 = new Estudiante();
+
+        // Asignar valores a los atributos
+        estudiante1.nombre = "Ilse Rueda";
+        estudiante1.edad = 23;
+
+        // Llamar al método saludar() del objeto
+        estudiante1.saludar();
+    }
+}
