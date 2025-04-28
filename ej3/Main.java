02fcafab9d17a86bcfe8a161f1467d0297d743b0
Ilse <iruedah1205@gmail.com>
2025-04-27 21:06:03 -0600

    Add files via upload

---
+++ ej3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args){
+
+        // Crear un objeto de tipo Producto utilizando el constructor
+        Producto prod1 = new Producto("Celular", 18966.6);
+
+        // Llamar al método para mostrar la información del producto
+        prod1.mostrarInfo();
+    }
+}
